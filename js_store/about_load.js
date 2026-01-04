8521fba608e23e50d8594d8e8ca69ff0ca589124
JSHP Shillong <jshpshillong1993@gmail.com>
2026-01-04 21:43:17 +0530

    Update about_load.js

--- js_store/about_load.js
+++ js_store/about_load.js
@@ -1,4 +1,4 @@
- fetch("about_section.html")
+ fetch("https://jshp.online/main/about_section.html")
     .then(response => {
       if (!response.ok) {
         throw new Error("Network response was not ok");
@@ -12,4 +12,5 @@
       console.error("About section load error:", error);
       document.getElementById("aboutContent").innerHTML =
         "<p>Unable to load About section.</p>";
+
     });
